1991635269d5261be56fa07acbdfa41f866979d9
Chukwudi Ogbonna <chuks@Chuks-Macbook-Pro.local>
2023-02-12 09:56:44 -0500

    user.go updated

--- models/user.go
+++ models/user.go
@@ -3,11 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 
-type User struct{
-	ID primitive.ObjectID `bson:"_id"`
-	FirstName string
-	LastName string
-	Email string 
-	Password string
-
+type School struct{
+	ID primitive.ObjectID `bson:"id"`
+	Name string `json:"firstname"`
+	Location string`json:"location"`
+	Student Headcount int `json:"studentheadcount"`
+	Staff Headcount int `json:"staffheadcount"`
 }
