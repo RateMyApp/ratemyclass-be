a3b540e93a1c952a7bae649d077fcbbf38b2566f
Chukwudi Ogbonna <chuks@Chuks-Macbook-Pro.local>
2023-04-25 11:04:00 -0400

    added country.csv file

--- routers/routers.go
+++ routers/routers.go
@@ -1,11 +1,11 @@
 package routers
 
 import "go.uber.org/fx"
-
+// interface method implemented by all routes, this adds the route, and handler function to the router group
 type Router interface {
 	ExecRoutes()
 }
-
+// helper function
 func AsRouter(f interface{}) any {
 	return fx.Annotate(
 		f,
@@ -13,7 +13,7 @@ func AsRouter(f interface{}) any {
 		fx.ResultTags(`group:"routers"`),
 	)
 }
-
+// create a module, and instantiate our newhelloRouter and NewbyeRouter objects, resolve all depedencies needed by both objects
 var Module = fx.Module("routers",
 	fx.Provide(AsRouter(NewHelloRouter)),
 	fx.Provide(AsRouter(NewByeRouter)),
