ba2d9fae2ea342859d7d4f704bc690cd8e77034b
anjolaoluwaakindipe <anjyakindipe@gmail.com>
2023-04-29 19:17:03 -0400

    feat: added testify suite to userRepository test

--- repositories/user_repository_test.go
+++ repositories/user_repository_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"os"
 	"testing"
+
 	"github.com/ratemyapp/config"
 	"github.com/ratemyapp/dao"
 	"github.com/ratemyapp/models"
 	"github.com/ratemyapp/repositories"
-	"github.com/stretchr/testify/assert"
-	"gorm.io/gorm"
 	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
 )
+
 type UserRepositorySuite struct {
 	suite.Suite
 	appConfig      config.AppConfig
@@ -32,15 +33,15 @@ func (suite *UserRepositorySuite) SetupSuite() {
 	suite.userRepostory = repositories.NewUserRepository(suite.postgresClient)
 }
 
-func (suite *UserRepositorySuite) SetupTest(){
+func (suite *UserRepositorySuite) SetupTest() {
 	suite.transaction = suite.postgresClient.Db.Begin()
 }
 
-func(suite *UserRepositorySuite) TearDownTest(){
+func (suite *UserRepositorySuite) TearDownTest() {
 	suite.transaction.Rollback()
 }
 
-func (suite *UserRepositorySuite) TearDownSuite(){
+func (suite *UserRepositorySuite) TearDownSuite() {
 	for _, model := range *models.GetModels() {
 		suite.postgresClient.Db.Unscoped().Where("1 = 1").Delete(model)
 	}
@@ -48,48 +49,41 @@ func (suite *UserRepositorySuite) TearDownSuite(){
 	suite.postgresClient.Close(ctx)
 }
 
-func(suite *UserRepositorySuite) Test_FindUserByEmail_ReturnUser_WhenGivenAValidEmail(t *testing.T) {
-	
-	assert := assert.New(t)
+func (suite *UserRepositorySuite) Test_FindUserByEmail_ReturnUser_WhenGivenAValidEmail() {
 	user, err := suite.userRepostory.FindUserByEmail(suite.testUser.Email)
 
-	assert.Nil(err, "FindUserByEmail Error: Expected err to be nil")
-	assert.NotNil(user, "FindUserByEmail Error: Expected user to not be nil")
-	assert.NotEmpty(user.CreatedAt)
-	assert.NotEmpty(user.UpdatedAt)
-	assert.False(user.DeletedAt.Valid)
-	assert.Equal(suite.testUser.Password, user.Password)
-	assert.Equal(suite.testUser.Email, user.Email)
-	assert.Equal(suite.testUser.Firstname, user.Firstname)
-	assert.Equal(suite.testUser.Lastname, user.Lastname)
+	suite.Nil(err, "FindUserByEmail Error: Expected err to be nil")
+	suite.NotNil(user, "FindUserByEmail Error: Expected user to not be nil")
+	suite.NotEmpty(user.CreatedAt)
+	suite.NotEmpty(user.UpdatedAt)
+	suite.False(user.DeletedAt.Valid)
+	suite.Equal(suite.testUser.Password, user.Password)
+	suite.Equal(suite.testUser.Email, user.Email)
+	suite.Equal(suite.testUser.Firstname, user.Firstname)
+	suite.Equal(suite.testUser.Lastname, user.Lastname)
 }
 
-func (suite *UserRepositorySuite)Test_FindUserByEmail_ReturnNil_WhenGivenInvalidEmail(t *testing.T) {
-	assert := assert.New(t)
+func (suite *UserRepositorySuite) Test_FindUserByEmail_ReturnNil_WhenGivenInvalidEmail() {
 	user, err := suite.userRepostory.FindUserByEmail("notfound@gmail.com")
-	assert.Nil(user, "FindUserByEmail Error: Expected user to be Nil")
-	assert.Nil(err, "FindUserByEmail Error: Expected err to be Nil")
+	suite.Nil(user, "FindUserByEmail Error: Expected user to be Nil")
+	suite.Nil(err, "FindUserByEmail Error: Expected err to be Nil")
 }
 
-func(suite *UserRepositorySuite)Test_SaveUser_ReturnNil_WhenGivenAUserToSave(t *testing.T) {
-	
-	assert := assert.New(t)
-
+func (suite *UserRepositorySuite) Test_SaveUser_ReturnNil_WhenGivenAUserToSave() {
 	newUser := models.User{Firstname: "TestFirstname", Lastname: "TestLastname", Email: "Test@email.com", Password: "Testpassword"}
 
 	err := suite.userRepostory.SaveUser(newUser)
-	assert.Nil(err, "SaveUser Error: Expected SaveUser to return no Error")
+	suite.Nil(err, "SaveUser Error: Expected SaveUser to return no Error")
 
 	foundUser, err := suite.userRepostory.FindUserByEmail(newUser.Email)
-	assert.NotNil(foundUser)
-	assert.Nil(err)
-	assert.Equal(newUser.Lastname, foundUser.Lastname)
-	assert.Equal(newUser.Firstname, foundUser.Firstname)
-	assert.Equal(newUser.Email, foundUser.Email)
-	assert.Equal(newUser.Password, foundUser.Password)
+	suite.NotNil(foundUser)
+	suite.Nil(err)
+	suite.Equal(newUser.Lastname, foundUser.Lastname)
+	suite.Equal(newUser.Firstname, foundUser.Firstname)
+	suite.Equal(newUser.Email, foundUser.Email)
+	suite.Equal(newUser.Password, foundUser.Password)
 }
 
 func TestUserRepositorySuite(t *testing.T) {
 	suite.Run(t, new(UserRepositorySuite))
 }
-
