ef7ddb8d7da46cc1a7371df0c7fd3a2eb9bf8285
anjolaoluwaakindipe <anjyakindipe@gmail.com>
2023-05-18 14:36:54 -0400

    misc: Committing router changes

--- routers/course_router.go
+++ routers/course_router.go
@@ -1,1 +1,12 @@
 package routers
+
+import "github.com/gin-gonic/gin"
+
+type courseRouter struct {
+	ginRouter *gin.Engine
+}
+
+func (self *courseRouter) createCourse() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+	}
+}
